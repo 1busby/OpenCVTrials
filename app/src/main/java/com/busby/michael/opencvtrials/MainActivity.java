b50cee35c05c833898f06d381d54b3a12f8de78e
Michael Busby <mbusbyIT@gmail.com>
2017-04-23 22:24:56 -0400

    First commit

---
+++ app/src/main/java/com/busby/michael/opencvtrials/MainActivity.java
@@ -0,0 +1,27 @@
+package com.busby.michael.opencvtrials;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+}
+
